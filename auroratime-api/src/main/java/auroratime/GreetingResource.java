d474aed35ba2952a7cbe476d4b3ed56bd07ad90c
shyauroratime <leefaker56@gmail.com>
2023-09-08 18:34:12 -0300

    feat: adding resteasy jackson mapper and dealing with quarkus config params

--- auroratime-api/src/main/java/auroratime/GreetingResource.java
+++ auroratime-api/src/main/java/auroratime/GreetingResource.java
@@ -4,13 +4,15 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import java.util.Map;
 
-@Path("/hello")
+@Path("/info")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> getInfo() {
+        Map<String, String> info = Map.of("project_name", "Aurora Time Project", "project_desc", "My personagel project");
+        return info;
     }
 }
